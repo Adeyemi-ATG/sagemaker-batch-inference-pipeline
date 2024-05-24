5dfee300d2816ff8fd85b8f6d271a0bc9c37aab8
Adeyemi-ATG <adeyemiakintonde@theambassadors.com>
2024-05-24 19:33:37 +0100

    .

--- main.tf
+++ main.tf
@@ -2,14 +2,15 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 # Terraform Backend Configuration is optional if you defined the backend in your runner.
+/*
 terraform {
   backend "s3" {
     bucket = "batch-scoring-pipeline-tf-state"
     key    = "test/test_state"
-    region = "us-east-1"
+    region = "eu-west-2"
   }
 }
-
+*/
 locals {
   tags                    = jsondecode(file("${path.module}/tags.json"))
   email_address           = var.email_recipient
