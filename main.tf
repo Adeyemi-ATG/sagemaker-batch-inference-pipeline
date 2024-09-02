0934d1f2b2c11c3a46dc825b5e45bedebe32bf73
Adeyemi-ATG <144015767+Adeyemi-ATG@users.noreply.github.com>
2024-09-02 16:01:36 +0100

    Updatess

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
 # Data sources to retrieve information about the current AWS account and region
-
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
-
-
-# Terraform configuration block to manage providers
+# Terraform configuration block to manage providers and backend
 terraform {
   required_providers {
     aws = {
@@ -13,9 +10,7 @@ terraform {
       version = "~> 4.0"  # Specify the AWS provider version range
     }
   }
-}
 
-terraform {
   backend "s3" {
     bucket = "terraform-state-bucket-mlops-prob-model"
     key    = "terraform.tfstate"
@@ -24,23 +19,12 @@ terraform {
   }
 }
 
-
 # AWS provider configuration
 provider "aws" {
-  #region = data.aws_region.current.name  # Automatically use the current region
   region = var.aws_region
 }
 
-# Terraform Backend Configuration is optional if you defined the backend in your runner.
-/*
-terraform {
-  backend "s3" {
-    bucket = "batch-scoring-pipeline-tf-state"
-    key    = "test/test_state"
-    region = "eu-west-2"
-  }
-}
-*/
+# Local values
 locals {
   tags                    = jsondecode(file("${path.module}/tags.json"))
   email_address           = var.email_recipient
